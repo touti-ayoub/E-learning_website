873df2b46ecbf23360bd8cbc92b9ff56d52852b6
IdrisSahnoun <idrissahnoun@gmail.com>
2025-02-19 11:09:04 +0100

    missing frontoffice template

---
+++ backend/gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package tn.esprit.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("microservice1", r-> r.path("/candidat/**").uri("lb://microservice1")).
+				build();
+	}
+}
