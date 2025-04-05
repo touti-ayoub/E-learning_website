7f2df03f991e2a68f9f9fe315f5f6e75cf2c6ec9
ayoub <titan.ayoub28@gmail.com>
2025-04-05 21:33:58 +0100

    replacing the old backoffice template

---
+++ frontend/backoffice/src/app/theme/layout/admin/nav-bar/nav-bar.component.ts
@@ -0,0 +1,51 @@
+// Angular import
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+// project import
+import { BerryConfig } from 'src/app/app-config';
+
+import { NavLeftComponent } from './nav-left/nav-left.component';
+import { NavLogoComponent } from './nav-logo/nav-logo.component';
+import { NavRightComponent } from './nav-right/nav-right.component';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [NavLogoComponent, NavLeftComponent, NavRightComponent, CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent {
+  // public props
+  @Output() NavCollapse = new EventEmitter<boolean>();
+  @Output() NavCollapsedMob = new EventEmitter<boolean>();
+  navCollapsed: boolean;
+  windowWidth: number;
+  navCollapsedMob: boolean;
+
+  // Constructor
+  constructor() {
+    this.windowWidth = window.innerWidth;
+    this.navCollapsed = this.windowWidth >= 1025 ? BerryConfig.isCollapse_menu : false;
+    this.navCollapsedMob = false;
+  }
+
+  // public method
+  navCollapse() {
+    if (this.windowWidth >= 1025) {
+      this.navCollapsed = !this.navCollapsed;
+      this.NavCollapse.emit();
+    }
+  }
+
+  navCollapseMob() {
+    if (this.windowWidth < 1025) {
+      this.NavCollapsedMob.emit();
+    }
+  }
+
+  toggleNavCollapse() {
+    this.NavCollapse.emit(true);
+  }
+}
